41f0cc1e1315ed30444d60cd77316f58c79e6da9
Keleko34 <gregory.guidero@gmail.com>
2017-07-05 17:13:57 +0200

    fix pathing issue on package creation

--- .tasks/tasks/init/init.js
+++ .tasks/tasks/init/init.js
@@ -2,7 +2,8 @@ var base = require('./../../base'),
     fs = require('fs'),
     stream = require('./../../.core/core'),
     replace = require('./../../.core/transforms/replace'),
-    append = require('./../../.core/transforms/append-prepend').append;
+    append = require('./../../.core/transforms/append-prepend').append,
+    latest_konnekt_version = '0.8.6';
 
 module.exports = function()
 {
@@ -13,7 +14,7 @@ module.exports = function()
     var _streamIndex = stream(global.taskrunner.global+"/tasks/init/template/index/*"),
         _streamConfig = stream(global.taskrunner.global+"/tasks/init/template/config/**/*"),
         _streamInit = stream("require('konnekt');\n",true),
-        _streamPackage = stream('{\n  \"name\": \"'+res.Title.toLowerCase().replace(/\s/g,'_')+'\",\n  \"version\": \"0.8.6\",\n  \"description\": \"'+res.Description+'\",\n  \"scripts\": {\n    \"init\":\"node init --task init\",\n    \"create\":\"node init --task create\",\n    \"build\":\"node init --task build\",\n    \"server\":\"node init --task server\",\n    \"group\":\"node init --task group\"\n  },\n  \"dependencies\":{\n    \"konnekt\":\"^0.8.6\"\n  }\n}',true),
+        _streamPackage = stream('{\n  \"name\": \"'+res.Title.toLowerCase().replace(/\s/g,'_')+'\",\n  \"version\": \"0.8.6\",\n  \"description\": \"'+res.Description+'\",\n  \"scripts\": {\n    \"init\":\"node init --task init\",\n    \"create\":\"node init --task create\",\n    \"build\":\"node init --task build\",\n    \"server\":\"node init --task server\",\n    \"group\":\"node init --task group\"\n  },\n  \"dependencies\":{\n    \"konnekt\":\"^'+latest_konnekt_version+'\"\n  }\n}',true),
         _finished = 0,
         _config = global.taskrunner.config.Tasks.init;
     
@@ -58,7 +59,7 @@ module.exports = function()
     .write('');
     
     _streamInit.rename(function(fileNameObject){
-      return global.taskrunner.base+'/init.js';
+      return '/init.js';
     })
     .onEnd(function(){
       _finished += 1;
@@ -67,7 +68,7 @@ module.exports = function()
     .write('');
     
     _streamPackage.rename(function(fileNameObject){
-      return global.taskrunner.base+'/package.json';
+      return '/package.json';
     })
     .onEnd(function(){
       _finished += 1;
