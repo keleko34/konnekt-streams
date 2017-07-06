ae727043ee78a852bd3968adc918ea03dea9528b
Keleko34 <gregory.guidero@gmail.com>
2017-07-06 11:51:15 +0200

    fix mkdir having empty path arrays

--- .tasks/.core/mkdir/mkdir.js
+++ .tasks/.core/mkdir/mkdir.js
@@ -3,26 +3,29 @@ var fs = require('fs'),
 
 module.exports = function(dir,cb)
 {
-  function mkdir(path,dir,cb)
+  function mkdir(path,dir,nex)
   {
     fs.stat(path+'/'+dir,function(e){
       if(e && e.code === 'ENOENT')
       {
-        fs.mkdir(path+'/'+dir,(parseInt('0777', 8) & (~process.umask())),cb);
+        fs.mkdir(path+'/'+dir,(parseInt('0777', 8) & (~process.umask())),nex);
       }
       else
       {
-        cb();
+        nex();
       }
     })
   }
   
   mkdir.create = function(dir,cb)
   {
+    dir = dir.replace(/\\/g,'/');
     var _localPath = process.cwd().replace(/\\/g,'/').replace(/(\/node_modules.+)/g,''),
         _path = path.parse(dir).dir.replace(_localPath,''),
         _split = _path.split('/').filter(function(v){return (v !== undefined && v.length !== 0)}),
         _current = 0;
+    
+    if(_split[0] === undefined) _split[0] = '';
     
     function next()
     {
